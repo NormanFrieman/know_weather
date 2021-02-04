97a68b14d9f5e0f68cf49c8f9d4e6eb71056a32c
NormanFrieman <ezequiel-d.s@hotmail.com>
2021-02-03 23:04:47 -0300

    first commit

---
+++ frontend/src/app/weather-today/weather-today.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-today',
+  templateUrl: './weather-today.component.html',
+  styleUrls: ['./weather-today.component.css']
+})
+export class WeatherTodayComponent implements OnInit {
+  
+  temp: number;
+  
+  day: string;
+  
+  hour: string;
+
+  city: string;
+
+  constructor() {
+    this.temp = 12;
+    this.day = "Monday";
+    this.hour = "16:00";
+    this.city = "New York, NY, USA";
+  }
+
+  ngOnInit(): void {
+  }
+
+}
