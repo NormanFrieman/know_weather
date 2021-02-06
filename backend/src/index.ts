380315e8e8aef37a38c625d6f43b6cbf71ed7bc6
NormanFrieman <ezequiel-d.s@hotmail.com>
2021-02-06 00:02:10 -0300

    prepare for heroku deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import route from './routes';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(route);
 app.listen(PORT, () => {
     console.log(`[localhost:${PORT}]: Server is running`);
